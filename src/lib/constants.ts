9ef79b83e782c798a7583c1ea2c3312ed81346c2
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-08 22:59:06 +0530

    Add constants for navigation items

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const NAV_ITEMS = [
+  { label: "Home", href: "/" },
+  { label: "Services", href: "/services" },
+  { label: "Blog", href: "/blog" },
+  { label: "Projects", href: "/projects" },
+  { label: "Contact", href: "/contact" },
+];
