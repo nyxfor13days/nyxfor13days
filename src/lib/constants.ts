53ba7f45bac1015596dce3cbed97bc5bac5e52a7
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-17 14:36:49 +0530

    Remove unused NAV_ITEMS constant

--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const NAV_ITEMS = [
-  { label: "Home", href: "/" },
-  { label: "Services", href: "/services" },
-  { label: "Blog", href: "/blog" },
-  { label: "Projects", href: "/projects" },
-  { label: "Contact", href: "/contact" },
-];
