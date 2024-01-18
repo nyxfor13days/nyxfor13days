a82570be8011bc236c0058d55b5de8f91783c067
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-18 10:52:02 +0530

    Add container styles to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,10 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {},
   },
   plugins: [nextui()],
