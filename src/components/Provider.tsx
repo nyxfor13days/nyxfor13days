517a64b6120f982f0dadf151b73a71671e83dce9
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-18 23:49:19 +0530

    Add useRouter hook to Provider component

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/navigation";
 
 import { NextUIProvider } from "@nextui-org/react";
 
 export default function Provider({ children }: React.PropsWithChildren) {
+  const router = useRouter();
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
