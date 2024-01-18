5d017d55bba204e5d0e881be4cd8de9d432416f0
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-18 10:55:42 +0530

    Update fonts and add dark theme support

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { ThemeProvider } from "next-themes";
+
 import { NextUIProvider } from "@nextui-org/react";
 
 export default function Provider({ children }: React.PropsWithChildren) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+      >
+        {children}
+      </ThemeProvider>
+    </NextUIProvider>
+  );
 }
