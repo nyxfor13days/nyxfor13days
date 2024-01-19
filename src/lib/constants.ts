bdcaaad8b71e87b6e01218e6f9b73ef1e6ce0491
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-19 16:14:00 +0530

    Add about, experience, education, and certifications data

---
+++ src/lib/constants.ts
@@ -0,0 +1,149 @@
+export const about = [
+  {
+    title: "Philosophy",
+    description:
+      "My philosophy is to understand the problems of the world, define the problem and find multiple solutions for the problem.",
+  },
+  {
+    title: "Motivation",
+    description:
+      "I am motivated by the challenges I face. A challenge allows me to think more and stay fixated on the challenge itself. Whether I win or lose I am motivated to prove myself everytime.",
+  },
+  {
+    title: "Background",
+    description:
+      "I have 9 years of personal coding experience and 3 years of professional experience in designing and developing web applications. Recently I have also stepped into the world of android and ios development using flutter.",
+  },
+];
+
+export const experience = [
+  {
+    companyName: "Dhan AI",
+    type: "Full-Time",
+    position: "Frontend Engineer",
+    timeline: "November 2022 - Present",
+  },
+  {
+    companyName: "Waterhouse Foundation",
+    type: "Full-Time",
+    position: "CTO",
+    timeline: "May 2022 - Present",
+  },
+  {
+    companyName: "Pragyaware Informatics Private Limited",
+    type: "Full-Time",
+    position: "Frontend Developer",
+    timeline: "Feburary 2022 - December 2022",
+  },
+  {
+    companyName: "The Vison Studio",
+    type: "Self-Employed",
+    position: "CEO",
+    timeline: "May 2021 - October 2022",
+  },
+  {
+    companyName: "CT University",
+    type: "Internship",
+    position: "Student Ambassador",
+    timeline: "Feburary 2022 - July 2022",
+  },
+  {
+    companyName: "Whizrobo",
+    type: "Internship",
+    position: "Trainer",
+    timeline: "Feburary 2022 - June 2022",
+  },
+  {
+    companyName: "Mindplus Healthcare",
+    type: "Freelance",
+    position: "Designer",
+    timeline: "July 2021 - August 2021",
+  },
+  {
+    companyName: "Pragyaware Informatics Private Limited",
+    type: "Full-Time",
+    position: "Frontend Developer",
+    timeline: "June 2020 - June 2021",
+  },
+  {
+    companyName: "ZeroAi Technologies Inc",
+    type: "Full Time",
+    position: "Lead Designer",
+    timeline: "2019 - 2020",
+  },
+];
+
+export const education = [
+  {
+    institute: "CT University",
+    degree: "Bachelor of Technology (Specialised in Artificial Intelligence)",
+    timeline: "2019 - 2022",
+  },
+  {
+    institute: "Guru Nanak Dev Polytechnic College",
+    degree: "Diploma in Computer Science",
+    timeline: "2015 - 2018",
+  },
+  {
+    institute: "Jesus Sacred Heart School",
+    degree: "10th Grade",
+    timeline: "2015",
+  },
+];
+
+export const certifications = [
+  {
+    title: "Design and Develop a Website using Figma and CSS",
+    issued: "September 2022",
+    provider: "Coursera",
+    credentialURL: "https://www.coursera.org/account/accomplishments/certificate/MYLY7QX5T6HW",
+  },
+  {
+    title: "Introduction to Front-End Development",
+    issued: "September 2022",
+    provider: "Coursera",
+    credentialURL: "https://www.coursera.org/account/accomplishments/certificate/6BZSWAMAEYWB",
+  },
+  {
+    title: "Programming with JavaScript",
+    issued: "September 2022",
+    provider: "Coursera",
+    credentialURL: "https://www.coursera.org/account/accomplishments/certificate/332SYJZUV3FZ",
+  },
+  {
+    title: "Version Control",
+    issued: "September 2022",
+    provider: "Coursera",
+    credentialURL: "https://www.coursera.org/account/accomplishments/certificate/S6JFEY8GXEK9",
+  },
+  {
+    title: "CSS",
+    issued: "August 2022",
+    provider: "HackerRank",
+    credentialURL: "https://www.hackerrank.com/certificates/4b833b0fce93",
+  },
+  {
+    title: "Javascript (Basic)",
+    issued: "August 2022",
+    provider: "HackerRank",
+    credentialURL: "https://www.hackerrank.com/certificates/4b833b0fce93",
+  },
+  {
+    title: "Problem Solving (Basic}",
+    issued: "August 2022",
+    provider: "HackerRank",
+    credentialURL: "https://www.hackerrank.com/certificates/3a7f89f715b7",
+  },
+  {
+    title: "Python (Basic)",
+    issued: "August 2022",
+    provider: "HackerRank",
+    credentialURL: "https://www.hackerrank.com/certificates/987ee1626e50",
+  },
+  {
+    title: "React (Basic)",
+    issued: "August 2022",
+    provider: "HackerRank",
+    credentialURL: "https://www.hackerrank.com/certificates/2a06aa4d1a20",
+  },
+];
