06cb4fd5b6b7e66983b490f455db5198c7e33576
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-19 16:13:51 +0530

    Add SectionWrapper component

---
+++ src/components/SectionWrapper.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from "react";
+
+import { Divider } from "@nextui-org/divider";
+
+interface Props extends PropsWithChildren {
+  title: string;
+}
+
+export default function SectionWrapper({ title, children }: Props) {
+  const sectionClassNames = "py-8 grid grid-rows-auto md:grid-cols-3 lg:grid-cols-4 gap-4";
+
+  return (
+    <section className={sectionClassNames}>
+      <div className="space-y-4">
+        <h2 className="headings">{title}</h2>
+        <Divider className="lg:hidden" />
+      </div>
+
+      {children}
+    </section>
+  );
+}
