9e9e4b27e3857b67f96e5640c4eb76c40a319e81
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-19 18:21:06 +0530

    Update Footer text color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="h-12 flex items-center justify-center gap-4 text-xs font-bold">
+    <footer className="h-12 flex items-center justify-center gap-4 text-xs text-foreground/50">
       Harsh Sandhu © {new Date().getFullYear()}
     </footer>
   );
