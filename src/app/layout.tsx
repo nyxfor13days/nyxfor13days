0d8510d4d34d7d31cc41692f616ce7b742d4cc6e
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-21 21:50:41 +0530

    Add Toaster component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { PropsWithChildren } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Provider from "@/components/Provider";
+import { Toaster } from "@/components/Toaster";
 import { cn } from "@/lib/utils";
 import { Divider } from "@nextui-org/divider";
 import { Analytics } from "@vercel/analytics/react";
@@ -36,6 +37,7 @@ export default function RootLayout({ children }: Props) {
           <div className="relative py-12 min-h-[calc(100dvh-163px)]">{children}</div>
           <Divider />
           <Footer />
+          <Toaster />
         </Provider>
         <Analytics />
       </body>
