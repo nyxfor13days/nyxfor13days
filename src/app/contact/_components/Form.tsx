5aef1e53dd89074887878a7643684fa7dc31e1f9
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-21 20:54:41 +0530

    Add form validation and loading state to contact form

--- src/app/contact/_components/Form.tsx
+++ src/app/contact/_components/Form.tsx
@@ -1,40 +1,84 @@
 "use client";
 
+import { useState } from "react";
+import { z } from "zod";
+
 import { FormData, sendMessage } from "@/app/actions";
 import { useSetState } from "@mantine/hooks";
 import { Button, Input, Textarea } from "@nextui-org/react";
 
+const schema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  subject: z.string().optional(),
+  message: z.string(),
+});
+
 export default function Form() {
   const [inputs, setInputs] = useSetState<FormData>({});
+  const [errors, setErrors] = useState<Record<string, string[]>>({});
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = async () => {
-    await sendMessage(inputs);
-  };
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault(); // Prevent form from submitting normally
+    setLoading(true);
+
+    const validatedFields = schema.safeParse(inputs);
+    if (!validatedFields.success) {
+      setErrors(validatedFields.error.flatten().fieldErrors);
+      setLoading(false);
+      return; // Don't proceed if validation fails
+    }
+
+    try {
+      const res = await sendMessage(inputs).then((res) => res);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+      // Handle error here, e.g. show an error message
+    }
+
+    setLoading(false);
+  }
+
+  function checkError(id: string, errors: Record<string, string[]>) {
+    const isInvalid = id in errors;
+    const errorMessage = isInvalid ? errors[id] : "";
+    return { isInvalid, errorMessage };
+  }
+
+  const { isInvalid: isNameInvalid, errorMessage: nameErrorMessage } = checkError("name", errors);
+  const { isInvalid: isEmailInvalid, errorMessage: emailErrorMessage } = checkError("email", errors);
+  const { isInvalid: isMessageInvalid, errorMessage: messageErrorMessage } = checkError("message", errors);
 
   return (
-    <form
-      action={handleSubmit}
-      className="space-y-4"
-    >
+    <form className="space-y-4">
       <div className="grid grid-cols-2 gap-4">
         <Input
           isClearable
+          id="name"
           type="text"
           label="Name"
           value={inputs.name}
           onValueChange={(value: string) => setInputs({ name: value })}
+          isInvalid={isNameInvalid}
+          errorMessage={nameErrorMessage}
         />
 
         <Input
           isClearable
+          id="email"
           type="email"
           label="Email"
           value={inputs.email}
           onValueChange={(value: string) => setInputs({ email: value })}
+          isInvalid={isEmailInvalid}
+          errorMessage={emailErrorMessage}
         />
 
         <Input
           isClearable
+          id="subject"
           type="text"
           label="Subject"
           value={inputs.subject}
@@ -42,17 +86,22 @@ export default function Form() {
         />
 
         <Textarea
+          id="message"
           minRows={5}
           label="Message"
           description="If you want to hire me then please contact me personally on my email."
           value={inputs.message}
           onValueChange={(value: string) => setInputs({ message: value })}
+          isInvalid={isMessageInvalid}
+          errorMessage={messageErrorMessage}
         />
       </div>
 
       <Button
         type="submit"
         color="primary"
+        isLoading={loading}
+        onClick={handleSubmit}
       >
         Submit
       </Button>
