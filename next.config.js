e2cd0ebf58c4cdaca59961b00f9141b1325527a7
Nyxfor13days <nyxfor13days@gmail.com>
2024-01-22 19:18:59 +0530

    Add support for MDX files with remark-gfm plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import remarkGfm from "remark-gfm";
+import nextMDX from "@next/mdx";
 
-module.exports = nextConfig
+const withMDX = nextMDX({
+  extension: /\.(md|mdx)?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+module.exports = withMDX(nextConfig);
